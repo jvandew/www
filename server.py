22586ebb9c3f5cbd671234c00c7588534c77dd4a
Jacob Van De Weert <vandeweertj@gmail.com>
2014-11-16 22:31:14 -0500

    add initial version of tornado server

---
+++ server.py
@@ -0,0 +1,36 @@
+from tornado import options
+from tornado.httpserver import HTTPServer
+from tornado.ioloop import IOLoop
+from tornado.web import Application, RequestHandler
+
+class MainHandler(RequestHandler):
+  def get(self):
+    self.write('hi there!')
+
+def main():
+
+  options.define('port', default=8888, help='listen port', type=int)
+  options.define('static_path', default='static', help='static assert directory')
+  options.define('template_path', default='templates', help='template directory')
+  options.define('debug', default=True, help='start in debug mode', type=bool)
+  options.parse_command_line()
+  opts = options.options
+
+  handlers = [
+    (r'/', MainHandler),
+  ]
+
+  app = Application(
+    handlers,
+    debug=opts.debug,
+    static_path=opts.static_path,
+    template_path=opts.template_path,
+  )
+  server = HTTPServer(app)
+
+  server.listen(opts.port)
+  IOLoop.instance().start()
+
+if __name__ == '__main__':
+  main()
+
