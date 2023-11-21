65da0564985b6cedc00123fa06aa42398e7a5fea
Jacob Van De Weert <vandeweertj@gmail.com>
2023-11-21 18:00:15 -0500

    restyle server.py

--- server.py
+++ server.py
@@ -6,49 +6,50 @@ from tornado.ioloop import IOLoop
 from tornado.web import Application, RequestHandler
 
 
-DIR_NAME = path.dirname(__file__)
-
 class IndexHandler(RequestHandler):
-  def get(self, directory):
-    try:
-      self.render(path.join(directory, 'index.html'))
-    except IOError:
-      self.send_error(404)
+    def get(self, directory: str) -> None:
+      try:
+          self.render(path.join(directory, "index.html"))
+      except IOError:
+          self.send_error(404)
 
 
 class MainHandler(RequestHandler):
-  def get(self, template):
-    try:
-      self.render(template)
-    except IOError:
-      self.send_error(404)
+    def get(self, template: str) -> None:
+        try:
+            self.render(template)
+        except IOError:
+            self.send_error(404)
 
 
-def main():
+def parse_options() -> int:
+    options.define("port", default=8888, help="listen port", type=int)
+    options.define("static_path", default="static", help="static assert directory")
+    options.define("template_path", default="templates", help="template directory")
+    options.define("debug", default=False, help="start in debug mode", type=bool)
 
-  options.define('port', default=8888, help='listen port', type=int)
-  options.define('static_path', default='static', help='static assert directory')
-  options.define('template_path', default='templates', help='template directory')
-  options.define('debug', default=False, help='start in debug mode', type=bool)
-  options.parse_command_line()
-  opts = options.options
+    options.parse_command_line()
+    return options.options
 
-  handlers = [
-    (r'/(?P<directory>.*)/?', IndexHandler),
-    (r'/(?P<template>.*\.html)', MainHandler),
-  ]
 
-  app = Application(
-    handlers,
-    debug=opts.debug,
-    static_path=opts.static_path,
-    template_path=opts.template_path,
-  )
-  server = HTTPServer(app)
+def main() -> None:
+    opts = parse_options()
 
-  server.listen(opts.port)
-  IOLoop.instance().start()
+    app = Application(
+        handlers=[
+            (r"/(?P<directory>.*)/?", IndexHandler),
+            (r"/(?P<template>.*\.html)", MainHandler),
+        ],
+        debug=opts.debug,
+        static_path=opts.static_path,
+        template_path=opts.template_path,
+    )
+    server = HTTPServer(app)
 
-if __name__ == '__main__':
-  main()
+    server.listen(opts.port)
+    IOLoop.instance().start()
+
+
+if __name__ == "__main__":
+    main()
 
